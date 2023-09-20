7ef428b2f0f96a1c3d0edefcc31f0317b2293d8b
Abboskhan3 <abbosnewz1@gmail.com>
2023-09-20 17:47:42 +0300

    Add User Dto

---
+++ src/Service/DTOs/Users/UserLoginDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Service.DTOs.Users;
+
+public class UserLoginDto
+{
+    [Required]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    public string Password { get; set; } = string.Empty;
+}
