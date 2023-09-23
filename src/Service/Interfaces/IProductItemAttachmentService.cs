e548f36bd6df9c9e7129efee9c8429b3b81d1d7f
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-09-23 19:16:14 +0500

    Add ProductItemAttachment and ProductAttachment Services

--- src/Service/Interfaces/IProductItemAttachmentService.cs
+++ src/Service/Interfaces/IProductItemAttachmentService.cs
@@ -1,5 +1,12 @@
-﻿namespace Service.Interfaces;
+﻿using Service.DTOs.ProductItemAttachments;
+
+namespace Service.Interfaces;
 
 public interface IProductItemAttachmentService
 {
+    Task<ProductItemAttachmentResultDto> CreateAsync(ProductItemAttachmentCreationDto dto);
+    Task<ProductItemAttachmentResultDto> UpdateAsync(ProductItemAttachmentUpdateDto dto);
+    Task<bool> DeleteAsync(long id);
+    Task<ProductItemAttachmentResultDto> GetByIdAsync(long id);
+    Task<IEnumerable<ProductItemAttachmentResultDto>> GetAllAsync();
 }
