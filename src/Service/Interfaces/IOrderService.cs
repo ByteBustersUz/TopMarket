ac94548da65ecfd4aadcaaefbba3f5f252b96303
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-10-12 20:43:29 +0500

    Add order not implement service

--- src/Service/Interfaces/IOrderService.cs
+++ src/Service/Interfaces/IOrderService.cs
@@ -1,5 +1,12 @@
-﻿namespace Service.Interfaces;
+﻿using Service.DTOs.Orders;
+
+namespace Service.Interfaces;
 
 public interface IOrderService
 {
+    Task<OrderResultDto> CreateAsync(OrderCreationDto dto);
+    Task<OrderResultDto> UpdateAsync(OrderUpdateDto dto);
+    Task<bool> DeleteAsync(long id);
+    Task<OrderResultDto> GetByIdAsync(long id);
+    Task<IEnumerable<OrderResultDto>> GetAllAsync();
 }
--- src/Service/Services/OrderService.cs
+++ src/Service/Services/OrderService.cs
@@ -1,7 +1,65 @@
-﻿using Service.Interfaces;
+﻿using AutoMapper;
+using Data.IRepositories;
+using Domain.Entities.OrderStatuses;
+using Domain.Entities.OrderFolder;
+using Service.Interfaces;
+using Service.DTOs.Orders;
+using Service.Exceptions;
 
 namespace Service.Services;
 
 public class OrderService : IOrderService
 {
+    private readonly IMapper mapper;
+    private readonly IRepository<Order> orderRepository;
+    private readonly IRepository<OrderStatus> addressRepository;
+    private readonly IRepository<OrderStatus> orderStatusRepository;
+    private readonly IRepository<ShippingMethod> shippingMethodRepository;
+
+    public OrderService(
+        IMapper mapper,
+        IRepository<Order> orderRepository,
+        IRepository<OrderStatus> addressRepository,
+        IRepository<OrderStatus> orderStatusRepository,
+        IRepository<ShippingMethod> shippingMethodRepository)
+    {
+        this.mapper = mapper;
+        this.orderRepository = orderRepository;
+        this.addressRepository = addressRepository;
+        this.orderStatusRepository = orderStatusRepository;
+        this.shippingMethodRepository = shippingMethodRepository;
+    }
+
+    public async Task<OrderResultDto> CreateAsync(OrderCreationDto dto)
+    {
+        var existAddress = await addressRepository.GetAsync(a => a.Id.Equals(dto.AddressId))
+            ?? throw new NotFoundException($"This address was not found with = {dto.AddressId}");
+
+        var existOrderStatus = await orderStatusRepository.GetAsync(o => o.Id.Equals(dto.StatusId))
+            ?? throw new NotFoundException($"This orderStatus was not found with = {dto.StatusId}");
+        
+        var existShippingMethod = await shippingMethodRepository.GetAsync(sh => sh.Id.Equals(dto.ShippingMethodId))
+            ?? throw new NotFoundException($"This shippingMethod was not found with = {dto.ShippingMethodId}");
+
+        throw new NotImplementedException();
+    }
+    public Task<OrderResultDto> UpdateAsync(OrderUpdateDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<OrderResultDto>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<OrderResultDto> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
 }
