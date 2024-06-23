d48cf0f9480878504ec1c771d93c2259678d8b60
Saidkamolxon <saidkamolxon1997@gmail.com>
2024-06-24 04:47:14 +0500

    Type fixed

--- src/Domain/Commons/Auditable.cs
+++ src/Domain/Commons/Auditable.cs
@@ -4,6 +4,6 @@ public class Auditable
 {
     public long Id { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime? UpdatetAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
     public bool IsDeleted { get; set; } = false;
 }
